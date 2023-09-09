477b9859f3711a42147ae217b002a3c11171f7e0
farhaan <farhaan.khan@xaviers.edu.in>
2023-09-10 00:12:04 +0530

    minor changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,6 @@ vim.opt.mouse = ""
 
 
 
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='gray', bold=true })
-vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='gray', bold=true })
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'gray', bold = true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg = 'yellow', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'gray', bold = true })
