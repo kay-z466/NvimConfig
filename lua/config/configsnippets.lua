3ba3990a04296b5018592cdcbfaa01a5ca2bb395
farhaan <farhaan.khan@xaviers.edu.in>
2023-09-12 13:25:23 +0530

    Changes to be committed: 	modified:   init.lua 	modified:   lua/config/LuaSnip/all.lua 	modified:   lua/config/configsnippets.lua

--- lua/config/configsnippets.lua
+++ lua/config/configsnippets.lua
@@ -22,3 +22,4 @@ require("luasnip").config.set_config({ -- Setting LuaSnip config
   -- Use Tab (or some other key if you prefer) to trigger visual selection
   store_selection_keys = "<Tab>",
 })
+--
