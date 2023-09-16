4888b754ca254bf0ba6d9df15a7efe73d917b552
farhaan <farhaan.khan@xaviers.edu.in>
2023-09-16 11:36:24 +0530

    LSP fix for pyright + minor tweaks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,15 +4,15 @@ vim.opt.termguicolors = true                                        --enable all
 vim.opt.laststatus = 3                                              -- size of status line
 vim.g.mapleader = ' '                                               -- set global leader key to space (also defined in keymaps.lua)
 vim.opt.mouse = "a"                                                 --enable mouse mode
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'gray', bold = true }) --set color for line numbers(I find theme colors to be a bit hard to read)
-vim.api.nvim_set_hl(0, 'LineNr', { fg = 'yellow', bold = true })    --set color for line numbers(I find theme colors to be a bit hard to read)
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'gray', bold = true }) --set color for line numbers(I find theme colors to be a bit hard to read)
 vim.opt.cursorline = true                                           -- Enable highlighting of the current line
 vim.opt.ignorecase = true                                           -- Ignore case
 vim.opt.smartcase = true                                            -- Don't ignore case with capitals
 vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 
 
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#6E808A', bold = true }) --set color for line numbers(I find theme colors to be a bit hard to read)
+vim.api.nvim_set_hl(0, 'LineNr', { fg = 'yellow', bold = true })    --set color for line numbers(I find theme colors to be a bit hard to read)
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#6E808A', bold = true }) --set color for line numbers(I find theme colors to be a bit hard to read)
 
 
 ---------------------------------------------------LINE JUST TO MAKE JUMPING EASIER---------------------------------------------------------------
