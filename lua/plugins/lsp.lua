7fc64357db26bcad2df1597ad8fd92b4fd23552f
farhaan <farhaan.khan@xaviers.edu.in>
2023-09-23 11:24:59 +0530

    added line indent + formatted code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,84 +1,82 @@
-
-
 vim.keymap.set("n", "<space>d", vim.diagnostic.open_float)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 
 vim.api.nvim_create_autocmd("LspAttach", {
-   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-   callback = function(ev)
-      vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-      local opts = { buffer = ev.buf }
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-   end,
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	end,
 })
 
 local languages = {
-   -- "pyright",
-   "texlab",
+	-- "pyright",
+	"texlab",
 }
 
 return {
-   {
-      "neovim/nvim-lspconfig",
-      dependencies = { "hrsh7th/cmp-nvim-lsp" },
-      config = function()
-         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-         require("lspconfig").lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-               Lua = {
-                  diagnostics = {
-                     globals = { "vim", "describe", "it" },
-                  },
-               },
-            },
-         })
-         require("lspconfig").pylsp.setup({
-         capabilities=capabilities,
-         })
-         require("lspconfig").tailwindcss.setup({
-            on_attach = function()
-               require("tailwindcss-colors").buf_attach(0)
-            end,
-         })
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("lspconfig").lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim", "describe", "it" },
+						},
+					},
+				},
+			})
+			require("lspconfig").pylsp.setup({
+				capabilities = capabilities,
+			})
+			require("lspconfig").tailwindcss.setup({
+				on_attach = function()
+					require("tailwindcss-colors").buf_attach(0)
+				end,
+			})
 
-         for _, language in pairs(languages) do
-            require("lspconfig")[language].setup({
-               capabilities = capabilities,
-            })
-         end
+			for _, language in pairs(languages) do
+				require("lspconfig")[language].setup({
+					capabilities = capabilities,
+				})
+			end
 
-         vim.keymap.set(
-            "n",
-            "<Leader>fa",
-            ":EslintFixAll<CR>",
-            { noremap = true, silent = true }
-         )
+			vim.keymap.set(
+				"n",
+				"<Leader>fa",
+				":EslintFixAll<CR>",
+				{ noremap = true, silent = true }
+			)
 
-         vim.lsp.handlers["textDocument/publishDiagnostics"] =
-            vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-               virtual_text = false,
-            })
-      end,
-   },
-   {
-      "williamboman/mason.nvim",
-      opts = {},
-   },
-   {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-         ensure_installed = {
-            "lua_ls",
-            -- "pyright",
-         },
-      },
-   },
+			vim.lsp.handlers["textDocument/publishDiagnostics"] =
+			    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+				    virtual_text = false,
+			    })
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				-- "pyright",
+			},
+		},
+	},
 }
