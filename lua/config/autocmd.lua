eb09cea5f3acde1a4873dc9fb7c4195515d18606
farhaan <farhaan.khan@xaviers.edu.in>
2023-10-07 16:38:47 +0530

    fixed breaking changes in indentline

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -3,7 +3,13 @@
 vim.cmd [[
   augroup IndentBlankLineToggle
     autocmd!
-    autocmd BufEnter *.txt,*.md :IndentBlanklineToggle
+    autocmd BufEnter *.txt,*.md,*.tex :IBLToggle
   augroup END
 ]]
 
+-- vim.cmd [[
+--   augroup IndentBlankLineToggle
+--     autocmd!
+--     autocmd BufLeave *.txt,*.md,*.tex :IBLToggle
+--   augroup END
+-- ]]
--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,5 +1,6 @@
 return{
-   { "lukas-reineke/indent-blankline.nvim" },
+   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+   -- { "lukas-reineke/indent-blankline.nvim" },
    -- config = function()
 	--    require("indent_blankline").setup()
    -- end
