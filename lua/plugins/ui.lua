8a5c39aad215a3cfac15b684f4c02d358544d95d
anon <kayzero21@gmail.com>
2024-04-26 18:21:56 +0530

    removed themes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,13 +7,13 @@ return {
 		end,
 	},
 	{
-		"ellisonleao/gruvbox.nvim",
+		-- "ellisonleao/gruvbox.nvim",
 		--config = function()
 		--vim.cmd("colorscheme gruvbox")
 		--end
 	},
 	{
-		"catppuccin/nvim",
+		-- "catppuccin/nvim",
 		-- config = function()
 		-- 	vim.cmd("colorscheme catppuccin-frappe")
 		-- end
