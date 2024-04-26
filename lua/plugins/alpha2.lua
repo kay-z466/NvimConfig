31dcc3e48f31e09f586045d6e09aab5d344ba07a
anon <kayzero21@gmail.com>
2024-04-26 19:38:32 +0530

    test lazy auth

--- lua/plugins/alpha2.lua
+++ lua/plugins/alpha2.lua
@@ -66,14 +66,12 @@ HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 				local stats = require("lazy").stats()
 				local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
 				dashboard.section.footer.val = "⚡ Neovim loaded " ..
-					stats.count .. " plugins in " .. ms .. "ms"
+				    stats.count .. " plugins in " .. ms .. "ms"
 				pcall(vim.cmd.AlphaRedraw)
 			end,
 		})
 	end,
 }
-
-
 --           //                 /#
 --       ///////               ####
 --    ////////////             ######
